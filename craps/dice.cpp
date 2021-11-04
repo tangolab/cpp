2cad532468459ee1d4c44f3932811f2fe1c0edef
Lorrettus <Lorrettus@LAPTOP-NDT6M2NU>
2021-11-03 21:40:53 -0400

    simplified

--- craps/dice.cpp
+++ craps/dice.cpp
@@ -6,19 +6,7 @@
 
 using namespace std;
 int coinToss() {
-    int randNumber = rand();
-    cout << "Random number " << randNumber << endl;
-    // divide the random number by two and obtain the remainder; 
-    int remainder = randNumber % 2;
-
-    cout << "remainder " << remainder << endl;
-    // it will either be 0 or 1; so add a 1 to it; 
-
-    int return_value = remainder + 1;
-    cout << "return value " << return_value << endl;
-    // now the result will be either a 1 or 2
-    return return_value;
-
+    return 1 + rand() % 2 ;
 }
 
 int main()
